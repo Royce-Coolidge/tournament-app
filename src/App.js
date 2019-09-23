34bfb36086ac92e43e60a2b44dc61c642ba80fbc
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-23 11:46:15 +0100

    added initial.js with playerName and numOfPlayers properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HomePage from './Components/HomePage/HomePage';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
    <>
-   <HomePage />
+    <Router>
+      <Route exact path="/" component={ HomePage } />
+    </Router>
    </>
   );
 }
