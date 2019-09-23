9703e629427ab7847c1aa122fb285e779e08ab3e
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-23 12:49:27 +0100

    import provider component from react-redux and wrapped it around entire app

--- src/Data/store.js
+++ src/Data/store.js
@@ -2,15 +2,12 @@ import { createStore } from "redux";
 import reducer from "./reducer";
 import initial from "./initial";
 
-const store = createStore( reducer, initial);
+const store = createStore( 
+    reducer, 
+    initial,
+    window.__REDUX_devtools_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
-// subscribe to any changes in state
-store.subscribe(() => {
-// get the current state using the getState method
-// we can get the state, but not update it    
-    let state = store.getState();
-    console.log(state.numOfPlayers)
-});
 
-store.dispatch({ type: "increment" });
+export default store;
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./Data/store"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// wrap Router with provider - connects the entire app to the store, store can be accessed anywhere in the app
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
